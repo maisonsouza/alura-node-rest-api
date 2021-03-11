85efeb29379bcf9b8d1ac75f17c224a50ea03ccb
maison.souza@prodam.am.gov.br <Maison1528>
2021-03-11 11:44:55 -0400

    Criar a primeira rota
    Adicionando o nodemon como dependencia de desenvolvimento

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get('/', function (req, res) {
     res.send('Hello World')
 })
 app.get('/atendimentos', function (req, res) {
-    res.send('Você está na rota de atendimentos')
+    res.send('Você está na rota de atendimentos e está realizando um GET')
 })
 
 app.listen(3000,()=>console.log("Servidor rodando na porta 3000"));
