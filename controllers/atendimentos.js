6c13fe251ef3cd4cb85b859d0834b71f1a54e742
maison.souza@prodam.am.gov.br <Maison1528>
2021-04-04 22:49:28 -0400

    Refatorar nosso código
    Refatorando nosso codigo
    Adicionando data no atendimento
    Aplicando validações de entrada

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,8 +1,15 @@
+
+const Atendimento = require('../models/atendimentos')
+
 module.exports = app => {
     app.get('/atendimentos', function (req, res) {
-        res.send('Você está na rota de atendimentos e está realizando um GET')
+        res.send('Você está na rota de atendimentos e está realizando um GET');
     })
     app.post('/atendimentos', function (req, res) {
-        res.send('Você está na rota de atendimentos e está realizando um POST')
+        console.log('Enviando atendimento')
+        const atendimento = req.body
+        Atendimento.adiciona(atendimento, res)
     })
 }
+
+
