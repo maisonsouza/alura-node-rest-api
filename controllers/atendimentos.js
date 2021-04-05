d4f78d089abee56587038e3318ca9b1aa64f6c2a
maison.souza@prodam.am.gov.br <Maison1528>
2021-04-05 17:16:17 -0400

    Finalizar nossa API
    Aprendemos outros verbos HTTP: GET, PUT, PATCH e DELETE;
    Diferenciamos quando usar PUT e quando usar PATCH;
    Retornamos um objeto útil para o cliente.

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,14 +1,27 @@
-
 const Atendimento = require('../models/atendimentos')
 
 module.exports = app => {
-    app.get('/atendimentos', function (req, res) {
-        res.send('Você está na rota de atendimentos e está realizando um GET');
+    app.get('/atendimentos', (req, res) => {
+        Atendimento.lista(res)
+    })
+
+    app.get('/atendimentos/:id', (req,res) => {
+        const id = parseInt(req.params.id)
+        Atendimento.buscaPorID(id,res)
     })
     app.post('/atendimentos', function (req, res) {
         console.log('Enviando atendimento')
         const atendimento = req.body
         Atendimento.adiciona(atendimento, res)
+    })
+    app.patch('/atendimentos/:id', (req,res) => {
+        const id = parseInt(req.params.id)
+        const valores = req.body
+        Atendimento.altera(id,valores,res)
+    })
+    app.delete('/atendimentos/:id', (req,res) => {
+        const id = parseInt(req.params.id)
+        Atendimento.deleta(id,res)
     })
 }
 
